cf6639b3fda959850f523a8b363d7c671ba3952a
Xisco Sastre-Cabot <xiscosastre@gmail.com>
2024-04-28 11:56:31 +0200

    add qr loading

--- src/routes/(app)/orders/scan/+page.svelte
+++ src/routes/(app)/orders/scan/+page.svelte
@@ -3,8 +3,10 @@
 	import { Icon } from 'svelte-awesome';
 	import search from 'svelte-awesome/icons/search';
 	import { goto } from '$app/navigation';
+	import ProgressBar from '$lib/components/ProgressBar.svelte';
 
 	let result = '';
+	let loading = false;
 
 	function _onPermissionError() {
 		alert('Permiso de cámara necesario');
@@ -13,6 +15,7 @@
 
 	function _onResulted() {
 		if (result.length > 0) {
+			loading = true;
 			goto('/orders/' + result);
 		}
 	}
@@ -20,20 +23,24 @@
 
 <div class="px-2 pt-1 text-xl font-semibold">Escanear pedido</div>
 <div class="flex w-full flex-col place-content-center items-center gap-5">
-	<ScanQRCode
-		bind:scanResult={result}
-		enableQRCodeReaderButton={false}
-		options={{
-			onPermissionError: () => _onPermissionError(),
-			onResulted: () => _onResulted()
-		}}
-	/>
-	<button
-		class="variant-filled-warning btn w-full"
-		on:click={() => {
-			goto('/orders/search');
-		}}
-	>
-		<Icon class="mr-2" data={search} /> Introducir manualmente
-	</button>
+	{#if loading}
+		<ProgressBar text={'Cargando pedido...'} />
+	{:else}
+		<ScanQRCode
+			bind:scanResult={result}
+			enableQRCodeReaderButton={false}
+			options={{
+				onPermissionError: () => _onPermissionError(),
+				onResulted: () => _onResulted()
+			}}
+		/>
+		<button
+			class="variant-filled-warning btn w-full"
+			on:click={() => {
+				goto('/orders/search');
+			}}
+		>
+			<Icon class="mr-2" data={search} /> Introducir manualmente
+		</button>
+	{/if}
 </div>
