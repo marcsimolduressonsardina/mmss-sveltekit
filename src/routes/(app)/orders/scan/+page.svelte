e4211e91995d2e8a71ab5449ad8138a7b7ef85b1
Xisco Sastre-Cabot <xiscosastre@gmail.com>
2024-04-28 11:51:07 +0200

    update qr

--- src/routes/(app)/orders/scan/+page.svelte
+++ src/routes/(app)/orders/scan/+page.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
-	import { BarcodeScanner } from 'svelte-barcode-scanner';
+	import { ScanQRCode } from '@kuiper/svelte-scan-qrcode';
 	import { Icon } from 'svelte-awesome';
 	import search from 'svelte-awesome/icons/search';
 	import { goto } from '$app/navigation';
+
+	let result = '';
+
+	function _onPermissionError() {
+		alert('Permiso de cámara necesario');
+		location.reload();
+	}
+
+	function _onResulted() {
+		if (result.length > 0) {
+			goto('/orders/' + result);
+		}
+	}
 </script>
 
 <div class="px-2 pt-1 text-xl font-semibold">Escanear pedido</div>
 <div class="flex w-full flex-col place-content-center items-center gap-5">
 	<div class="barcode-scanner">
-		<BarcodeScanner on:detect={(event) => alert(JSON.stringify(event))} />
+		<ScanQRCode
+			bind:scanResult={result}
+			enableQRCodeReaderButton={false}
+			options={{
+				onPermissionError: () => _onPermissionError(),
+				onResulted: () => _onResulted()
+			}}
+		/>
 	</div>
 	<button
 		class="variant-filled-warning btn w-full"
