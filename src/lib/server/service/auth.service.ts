97867dcf037a209880f4eb9f8d47d0cb5b7a0c02
Xisco Sastre-Cabot <xiscosastre@gmail.com>
2024-05-11 12:55:13 +0200

    fix auth service

--- src/lib/server/service/auth.service.ts
+++ src/lib/server/service/auth.service.ts
@@ -2,12 +2,18 @@ import type { AppUser, CustomSession, StaticUser } from '$lib/type/api.type';
 
 export class AuthService {
 	public static generateUserFromAuth(session?: CustomSession): AppUser | undefined {
-		if (session == null) return undefined;
-		const user = session.user;
-		if (user == null || session.userMetadata.storeId == null) return undefined;
+		if (
+			session == null ||
+			session.user == null ||
+			session.userMetadata == null ||
+			session.userMetadata.storeId == null
+		) {
+			return undefined;
+		}
+
 		return {
-			id: user.id!,
-			name: user.name!,
+			id: session.user.id!,
+			name: session.user.name!,
 			storeId: session.userMetadata.storeId,
 			priceManager: session.userMetadata.priceManager ?? false
 		};
